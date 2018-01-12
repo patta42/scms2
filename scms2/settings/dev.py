a77ce88a9f37d590ba01640328d634d73169928e
patta42 <patta42@users.noreply.github.com>
2018-01-12 12:26:56 +0100

    Added ALLOWED_HOSTS, removed SECRET_KEY
    SECRET_KEY moved to local.py

--- scms2/settings/dev.py
+++ scms2/settings/dev.py
@@ -5,11 +5,16 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'py009wkdx4&w5wvk8bmkd8+9#eb@&y&2q^6nuuetvycpm)1dka'
-
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ALLOWED_HOSTS = [
+    'dinschede.localhost',
+    'gloesingen.localhost',
+    'oeventrop.localhost',
+    'ssg.localhost',
+    'bruderschaft.localhost',
+    'localhost',
+]
 
 
 try:
